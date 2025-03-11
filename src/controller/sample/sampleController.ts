eae501493dbaab6ec2abeb3b0a3b2398d703f4e3
nishant-2212 <nishant.kumar@batterysmart.in>
2025-03-11 17:49:45 +0530

    feat- testing of redis

--- src/controller/sample/sampleController.ts
+++ src/controller/sample/sampleController.ts
@@ -1,5 +1,8 @@
 import SampleTable from "../../database/postgres/models/sampleTable";
+import { redisClient } from "../../database/redis";
 import { SUCCESS, successResponse } from "../../utils/express/helpers";
+import { getAsync, setExAsync } from "../../utils/redis";
+import { uuidGenerator } from "../../utils/uuidGenerator";
 
 const sampleController = async (req, res, next) => {
   const { name } = req.body;
@@ -11,7 +14,23 @@ const sampleController = async (req, res, next) => {
     });
 
     const req = await SampleTable.findAll({ where: { name } });
-    return successResponse(SUCCESS.fetchSuccess, { req }, res);
+
+    const dataSet = await setExAsync({
+      client: redisClient,
+      key: `sampleKey:${x.id}`,
+      seconds: 60,
+      value: {
+        name: x.name,
+        value: uuidGenerator(),
+      },
+    });
+
+    const rDaata = await getAsync({
+      client: redisClient,
+      key: `sampleKey:${x.id}`,
+    });
+
+    return successResponse(SUCCESS.fetchSuccess, { req, rDaata }, res);
   } catch (error) {
     next(error);
   }
